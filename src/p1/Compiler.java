293a471057da61ba5740ef12272f17c9082004c0
bennibjorn <bennibjorn@hotmail.com>
2015-02-15 16:49:59 +0000

    Compiler and Lexer regex things

--- src/p1/Compiler.java
+++ src/p1/Compiler.java
@@ -1,5 +1,7 @@
 package p1;
 
 public class Compiler {
-
+	Lexer myLexer = new Lexer();
+	Parser myParser = new Parser(myLexer);
+	myParser.parse();
 }
--- src/p1/Lexer.java
+++ src/p1/Lexer.java
@@ -1,9 +1,42 @@
 package p1;
 
+import java.util.regex.*;
+
 public class Lexer {
 	public Token nextToken() {
 		Token token = null;
 		
+
+		// Regex define things
+		String INTstring = "[0-9]+";
+		String IDstring = "[A-Za-z]+";
+		String ENDstring = "end";
+		String PRINTstring = "print";
+		String PLUSstring = "+";
+		String MINUSstring = "-";
+		String MULTstring = "*";
+		String LPARENstring = "{";
+		String RPARENstring = "}";
+		//String ASSIGNstring = "=";
+		String SEMICOLstring = ";";
+		
+		// Regex Patterns
+		Pattern INT = Pattern.compile(INTstring);
+		Pattern ID = Pattern.compile(IDstring);
+		Pattern END = Pattern.compile(ENDstring);
+		Pattern PRINT = Pattern.compile(PRINTstring);
+		Pattern PLUS = Pattern.compile(PLUSstring);
+		Pattern MINUS = Pattern.compile(MINUSstring);
+		Pattern MULT = Pattern.compile(MULTstring);
+		Pattern LPAREN = Pattern.compile(LPARENstring);
+		Pattern RPAREN = Pattern.compile(RPARENstring);
+		Pattern SEMICOL = Pattern.compile(SEMICOLstring);
+		
+		
+		
 		return token;
+	}
+	private void read() {
+		
 	}
 }
