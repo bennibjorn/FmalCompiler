6d79e0b3eeab579c8763f6e163b95bb1acc384a3
Katur7 <grimurk@gmail.com>
2015-02-15 22:45:18 +0000

    Compiler reads input and sends as a string to Lexer

--- src/p1/Compiler.java
+++ src/p1/Compiler.java
@@ -1,9 +1,30 @@
 package p1;
 
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Compiler {
 	public static void main(String[] args) {
-		Lexer myLexer = new Lexer();
+		Lexer myLexer = new Lexer(readInput());
 		Parser myParser = new Parser(myLexer);
 		myParser.parse();
+	}
+	
+	private static String readInput() {
+		String input = "";
+		try{
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			String line = "";
+	 
+			while((line=br.readLine()) != null) {
+				input += line + "\n";
+			}
+		} catch(IOException io){
+			io.printStackTrace();
+			System.exit(0);
+		}
+		return input;
 	}
 }
