6f64a0afb3aaf196157ca22916cac8d3f61e2ad5
bennibjorn <bennibjorn@hotmail.com>
2015-02-15 16:08:21 +0000

    Added zhe zhings given in pdf to token

--- src/p1/Token.java
+++ src/p1/Token.java
@@ -1,5 +1,24 @@
 package p1;
 
 public class Token {
-
+	
+	enum TokenCode {
+		ID,
+		ASSIGN,
+		SEMICOL,
+		INT,
+		PLUS,
+		MINUS,
+		MULT,
+		LPAREN,
+		RPAREN,
+		PRINT,
+		END,
+		ERROR
+	}
+	
+	String lexeme;
+	TokenCode tCode;
+	
+	
 }
