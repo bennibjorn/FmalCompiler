d145bfd4dac3b634d0f0d2988e960b84fe2e00c9
Katur7 <grimurk@gmail.com>
2015-02-15 16:54:41 +0000

    Little work on Parser

--- src/p1/Parser.java
+++ src/p1/Parser.java
@@ -1,5 +1,19 @@
 package p1;
 
-public class Parser {
+import java.util.Stack;
 
+public class Parser {
+	private Lexer lexer = new Lexer();
+	private Token currToken = new Token();
+	
+	public Parser(Lexer lex) {
+		lexer = lex;
+	}
+	
+	public void parse() {
+		currToken = lexer.nextToken();
+	}
+	
+	Stack<Token> s = new Stack<Token>();
+	
 }
