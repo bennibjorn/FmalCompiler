7ceee2095bf7c36542a463cfea85797be4b9c801
bennibjorn <bennibjorn@hotmail.com>
2015-02-17 04:52:58 +0000

    After a lot of testing, figured this shit out and put a TODO for tomorrow, now: Sleep!

--- src/p1/Lexer.java
+++ src/p1/Lexer.java
@@ -7,35 +7,39 @@ import java.util.regex.*;
 public class Lexer {
 	
 	private String[] lex;
-	private String[] patterns;
+	private String[] stringPattern;
 	private String inputstring;
 	private Pattern pattern[];
+	private int check;
 	
 	public Lexer() {
 		System.out.println("Put a fucking string or a file into me you fuck");
 		inputstring = null;
+		nextToken();
 	}
 	
 	public Lexer(String input) {
 		inputstring = input;
 		System.out.println("inside lexer constructor"); //debug
 		lex = null;
+		check = 0;
 		
 		// String patterns for splitting
-		patterns = new String[10];
-		patterns[0] = "[0-9]+"; //INT
-		patterns[1] = "[A-Za-z]+"; //ID
-		patterns[2] = "end"; //END
-		patterns[3] = "print"; //PRINT
-		patterns[4] = "\\+"; //PLUS
-		patterns[5] = "\\-"; //MINUS
-		patterns[6] = "\\*"; //MULT
-		patterns[7] = "\\)"; //LPAREN
-		patterns[8] = "\\("; //RPAREN
-		patterns[9] = "\\;"; //SEMICOL
+		stringPattern = new String[11];
+		stringPattern[0] = "[0-9]+"; //INT
+		stringPattern[1] = "[A-Za-z]+"; //ID
+		stringPattern[2] = "end"; //END
+		stringPattern[3] = "print"; //PRINT
+		stringPattern[4] = "\\+"; //PLUS
+		stringPattern[5] = "\\-"; //MINUS
+		stringPattern[6] = "\\*"; //MULT
+		stringPattern[7] = "\\)"; //LPAREN
+		stringPattern[8] = "\\("; //RPAREN
+		stringPattern[9] = "\\;"; //SEMICOL
+		stringPattern[10] = "\\="; //ASSIGN
 		
 		// Compiled patterns for searching
-		pattern = new Pattern[10];
+		pattern = new Pattern[11];
 		pattern[0] = Pattern.compile("[0-9]+"); //INT
 		pattern[1] = Pattern.compile("[A-Za-z]+"); //ID
 		pattern[2] = Pattern.compile("end"); //END
@@ -46,7 +50,7 @@ public class Lexer {
 		pattern[7] = Pattern.compile("\\)"); //LPAREN
 		pattern[8] = Pattern.compile("\\("); //RPAREN
 		pattern[9] = Pattern.compile("\\;"); //SEMICOL
-		
+		pattern[10] = Pattern.compile("\\="); //ASSIGN
 		read();
 		}
 	
@@ -71,7 +75,7 @@ public class Lexer {
 	}
 	private void regexThings(String input) {
 		
-		Matcher next = null;
+		//Matcher next = null;
 		
 		/*
 		// Regex define things
@@ -101,25 +105,56 @@ public class Lexer {
 		Pattern SEMICOL = Pattern.compile(SEMICOLstring);
 		*/
 		//Pattern p = null;
-		lex = input.split(" "); //first split on spaces
-		// for each element in lex array after whitespace split
-		// check with switch if matcher.find() finds any of the Patterns defined
-		// and split on that element if found
-		
-		for (int i = 0; i < lex.length; i++) {
-			for (int u = 0; u < pattern.length; u++) {
-				next = pattern[u].matcher(lex[i]);
-				if (next.lookingAt()) {
-					lex[i].split(patterns[u]);
-					System.out.println(lex[i] + " splits on: " + patterns[u]);
-				}
-			}
+		lex = input.split("\\s"); //first split on whitespaces
+		if (patternChecker()) {
+			System.out.println("PatternChecker is done and returns true");
+		}
+		else {
+			System.out.println("Error in patternChecker, it returned false");
 		}
 		
+		//if lookingAt = true, checka á matches líka, ef það er true, return með næsta staki í lex
+		//If lookingAt = true, en matches != true, splitta með því patterni
 		//debug
 		System.out.println("Lex array: \n");
 		for (int i = 0; i < lex.length; i++) {
 			System.out.println(i + ": " + lex[i]);
 		}
+	}
+	/*
+	 * patternChecker checks if the element given matches only one pattern
+	 * in the array of patterns. If it does, it returns true and the while loop
+	 * checks the next one. If it returns false, the loop above will try to find
+	 * a pattern within the element and split it again until only one pattern remains.
+	 */
+	private boolean patternChecker() {
+		//Pattern p = Pattern.compile(lex[check]); //set the current pattern to the element given
+		Matcher m = null; 
+		
+		for (int i = 0; i < pattern.length; i++) { 
+			//m = p.matcher(patterns[i]); //sets the matcher pattern to number 'i' on the list of patterns
+			m = pattern[i].matcher(lex[check]);
+			//p.matches(pattern[i], elem);
+			if (m.lookingAt()) {
+				if (m.matches()) {
+					++check;
+					patternChecker(); //recursive call until everything returns true
+					return true;
+				}
+				else { //split on lex[check] with given pattern
+					lex[check].split(stringPattern[i]);
+					//lex[check] = (String[])lex[check].split(""); //debug purposes
+					//TODO:
+					/*
+					 * Nota ArrayList í stað String[] þar sem hægt er að stækka það
+					 * eftir þörfum sem er nauðsynlegt hér.
+					 * Hér þarf að stækka arrayið um einn, taka svo bæði split elements og setja þau
+					 * í nýja og það sem var splittað frá. Þannig ætti það að stækka!
+					 */
+				}
+			}
+		}
+		
+		return false;
 	}
 }
